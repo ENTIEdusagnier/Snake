f8d2dbcc4be74c5af7e243c4e66c3c2a52f1bb9c
Eduard Sagnier Gonzalez <eduard.sagnier@enti.cat>
2024-03-06 14:38:25 +0100

    nADA

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,18 +1,62 @@
 #define FRAME_RATE 150
+#define NUM_FILAS 10
+#define NUM_COLUMNAS 20
+#define SIM_LAT '|'
+#define SIM_TOP '-'
 
 #include <iostream>
 #include <thread>
 #include <chrono>
 
+using namespace std; //Permite no utilizar std::
+
+
+
+void sacar_mapa() {
+
+	char mapa[NUM_COLUMNAS][NUM_FILAS];
+	
+
+	for (int i = 0; i < NUM_COLUMNAS; i++) {
+		for (int j = 0; j < NUM_FILAS; j++) {
+
+			if (j == 0 || j == 10)
+			{
+				mapa[i][j] = SIM_LAT;
+			}
+			if (i > 0 && i < 20 && j > 0 && j < 10)
+			{
+				mapa[i][j] = ' ';
+			}
+			if (i == 0 || i == 20)
+			{
+				mapa[i][j] = SIM_TOP;
+			}
+		}
+		
+	}
+	for (int i = 0; i < NUM_COLUMNAS; i++) {
+		for (int j = 0; j < NUM_FILAS; j++) {
+
+			cout << mapa[i][j];       
+
+
+		}
+		cout << endl;
+	}
+
+}
 void main() {
 
-    bool bGameOver = false;
+	bool bGameOver = true;
 
-    //While game is not over execute game loop
-    while (!bGameOver) {
+	sacar_mapa();
 
-        std::cout << "a" << std::endl;
-        //Sleep main thread to control game speed execution
-        std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_RATE));
-    }
+	//While game is not over execute game loop
+	while (!bGameOver) {
+
+		std::cout << "a" << std::endl;
+		//Sleep main thread to control game speed execution
+		std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_RATE));
+	}
 }
